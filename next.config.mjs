91bc8187b449728950caea2a16398b0eaa69aac0
Naotaka Kanno <universe110423@gmail.com>
2022-10-25 14:34:07 +0900

    root pathの変更

--- next.config.mjs
+++ next.config.mjs
@@ -8,5 +8,5 @@ const isDev = process.env.NODE_ENV === "development";
 export default withPWA({
   eslint: { ignoreDuringBuilds: true },
   pwa: { dest: "public", disable: isDev },
-  serverRuntimeConfig: { GTM_ID: process.env.GTM_ID || "GTM_ID" },
+  serverRuntimeConfig: { GTM_ID: process.env.GTM_ID || "GTM_ID" }
 });
