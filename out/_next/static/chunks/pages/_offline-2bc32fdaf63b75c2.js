07ffb211433493112da609377b91e68f3765cc11
Naotaka Kanno <universe110423@gmail.com>
2022-10-25 13:53:33 +0900

    Next.jsのデプロイ

---
+++ out/_next/static/chunks/pages/_offline-2bc32fdaf63b75c2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[95],{4927:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_offline",function(){return _(4389)}])},4389:function(n,u,_){"use strict";_.r(u);var f=_(1676);u.default=f.default}},function(n){n.O(0,[84,676,774,888,179],(function(){return u=4927,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
